724af104f46dadcc4f83f4941fe9a4f2ca85dcf7
Luiz Guilherme <57669105+oLuizGuilherme@users.noreply.github.com>
2022-04-09 14:26:26 -0300

    Correções na classe LdeInt
    fix - Correção no método remove.
    feat - Criação do método concatena.

--- 3P/Estrutura de dados/ExerciciosDeAula/src/unidade02/LdeInt.java
+++ 3P/Estrutura de dados/ExerciciosDeAula/src/unidade02/LdeInt.java
@@ -28,11 +28,11 @@ public class LdeInt {
 	public int getnElem() {
 		return nElem;
 	}
-	
+
 	public boolean eVazia() {
 		return nElem == 0;
 	}
-	
+
 	public void insereUltimo(int info) {
 		NoDuplaInt novoNo = new NoDuplaInt(info);
 		if (eVazia()) {
@@ -46,7 +46,7 @@ public class LdeInt {
 		setUlt(novoNo);
 		nElem++;
 	}
-	
+
 	public void insereInicio(int info) {
 		NoDuplaInt novoNo = new NoDuplaInt(info);
 		if (eVazia()) {
@@ -60,7 +60,7 @@ public class LdeInt {
 		setPrim(novoNo);
 		nElem++;
 	}
-	
+
 	public NoDuplaInt pesquisa(int info) {
 		NoDuplaInt aux = getPrim();
 		while (aux != null && aux.getInfo() != info) {
@@ -68,28 +68,41 @@ public class LdeInt {
 		}
 		return aux;
 	}
-	
-	public NoDuplaInt pesquisaAoContrario(int info) {
-		NoDuplaInt aux = getUlt();
-		while (aux != null && aux.getInfo() != info) {
-			aux = aux.getAnt();
-		}
-		return aux;
-	}
-	
+
 	public boolean remove(int info) {
-		NoDuplaInt aux = getPrim();
-		while (aux != null && aux.getInfo() != info) {
-			aux = aux.getProx();
-		}
+		NoDuplaInt aux = pesquisa(info);
 		if (aux == null) {
 			return false;
 		}
-		aux.getAnt().setProx(aux.getProx());
-		aux.getProx().setAnt(aux.getAnt());
-		aux.setProx(null);
-		aux.setAnt(null);
+		if (nElem == 1) {
+			setPrim(null);
+			setUlt(null);
+		} else if (aux == getPrim()) {
+			setPrim(aux.getProx());
+			aux.getProx().setAnt(null);
+		} else if (aux == getUlt()) {
+			setUlt(aux.getAnt());
+			aux.getAnt().setProx(null);
+		} else {
+			aux.getAnt().setProx(aux.getProx());
+			aux.getProx().setAnt(aux.getAnt());
+		}
+		nElem--;
 		return true;
+	}
+
+	public void concatena(LdeInt lista1) {
+		if (!lista1.eVazia()) {
+			if (eVazia()) {
+				this.setPrim(lista1.getPrim());
+				this.setUlt(lista1.getUlt());
+				this.nElem = lista1.getnElem();
+			}
+			this.getUlt().setProx(lista1.getPrim());
+			lista1.getPrim().setAnt(this.getUlt());
+			this.setUlt(lista1.getUlt());
+			this.nElem += lista1.getnElem();
+		}
 	}
 
 }
